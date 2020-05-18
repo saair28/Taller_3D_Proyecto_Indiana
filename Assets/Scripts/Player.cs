1be1dcd8d313292b2d881420f3e45d5474e2e28d
saair28 <65096035+saair28@users.noreply.github.com>
2020-05-18 16:18:50 -0500

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1f;
+    public Camera MainCam;
+    private Vector3 Camforward;
+    private Vector3 CamRight;
+    private Vector3 playerMove;
+    private Vector3 playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(h, 0f, v);
+
+
+       
+        
+        
+    }
+
+}
