19db681eed249474cbe59aed271f9ae8265d6984
saair28 <65096035+saair28@users.noreply.github.com>
2020-05-18 17:26:02 -0500

    Add files via upload

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 1f;
-    public Camera MainCam;
-    private Vector3 Camforward;
-    private Vector3 CamRight;
-    private Vector3 playerMove;
-    private Vector3 playerInput;
+    public float rotationSpeed;
+    public float rotX;
+    public float rotY;
+    public float rotZ;
+    Rigidbody rb;
+
+    public bool isJump = false;
+    public float jumpForce = 5.0f;
+
+    private Camera mainCamera;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
@@ -25,10 +32,29 @@ public class Player : MonoBehaviour
 
         transform.position += new Vector3(h, 0f, v);
 
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
 
-       
-        
-        
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        jump();
+
+    }
+
+    void jump()
+    {
+        isJump = Input.GetButtonDown("Jump");
+
+        if (isJump)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
     }
 
 }
