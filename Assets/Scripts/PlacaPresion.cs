e57baae4074e445fa199d781318b52ad3f468679
bsm30 <65439485+bsm30@users.noreply.github.com>
2020-05-19 15:44:57 -0500

    Add files via upload

---
+++ Assets/Scripts/PlacaPresion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacaPresion : MonoBehaviour
+{
+    bool isclosed = false;
+
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (!isclosed)
+        {
+            isclosed = true;
+
+            door.transform.position -= new Vector3(0, 4.43f, 0);
+        }
+    }
+}
